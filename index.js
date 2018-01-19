75a221fb04282c57101b1ec8ffca25f6bd672721
Amit Kejriwal <techstunts@gmail.com>
2018-01-19 16:18:00 +0530

    Google api calls in async

--- index.js
+++ index.js
@@ -6,6 +6,15 @@ const port = 3000
 
 const requestHandler = (req, response) => {
     //console.log(req.method)
+    var timestamps = [];
+    var dt = new Date();
+    var totalTimeInMS =  new Date().getTime();
+
+    timestamps.push("Server received request: <i>" + dt.getMinutes() + ":" + dt.getSeconds() + ":" + dt.getMilliseconds() + "</i>");
+
+
+    response.setHeader("Access-Control-Allow-Origin", "*");
+
 
     var googleMapsClient = require('@google/maps').createClient({
         key: 'AIzaSyAnr61cPW34sINtAkzOQRtNSgia0ZE68VM'
@@ -17,7 +26,11 @@ const requestHandler = (req, response) => {
 
     if(req.method=='POST') {
         var body='';
+        //response.write("Step -1");
+        console.log("Step -1");
         req.on('data', function (data) {
+            //response.write("Step 0");
+            console.log("Step 0");
             body +=data;
         });
         req.on('end',function(){
@@ -25,7 +38,7 @@ const requestHandler = (req, response) => {
             //console.log(POST);
 
             origins = JSON.parse(POST['origins']);
-            destinations = JSON.parse(POST['destinations']);
+            all_destinations = JSON.parse(POST['destinations']);
             traffic_model = POST['trafficModel'];
 
             if(traffic_model == 'bestguess'){
@@ -33,38 +46,72 @@ const requestHandler = (req, response) => {
             }
 
 
-            var results;
+            var results = [];
 
             var asyncTasks = [];
+            const MAX_DESTINATIONS_PER_REQUEST = 25;
 
-            asyncTasks.push(function (callback) {
-                googleMapsClient.distanceMatrix({
-                    origins: origins,
-                    destinations: destinations,
-                    mode: 'driving',
-                    units: 'metric',
-                    departure_time: new Date(Date.now()),
-                    traffic_model: traffic_model
-                }, function (err, response) {
-                    //console.log(response);
-                    if (!err) {
-                        console.log(JSON.stringify(response.json.rows[0].elements));
-                        results = response.json.rows[0].elements;
-                        //response.end(JSON.stringify(response.json.rows[0].elements));
-                    }
-                });
-            });
+            console.log("Step 1");
+
+
+            i = 0;
+            while(i<all_destinations.length){
+                destinations = [];
+                console.log("Step 1.0.1");
+                console.log(i );
+                destinations = all_destinations.slice(i, i+MAX_DESTINATIONS_PER_REQUEST)
+                console.log(destinations );
+                (function(dest) {
+                    asyncTasks.push(function (callback) {
+                        console.log("Step 1.1");
+                        googleMapsClient.distanceMatrix({
+                            origins: origins,
+                            destinations: dest,
+                            mode: 'driving',
+                            units: 'metric',
+                            departure_time: new Date(Date.now()),
+                            traffic_model: traffic_model
+                        }, function (err, response) {
+                            var dt = new Date();
+                            timestamps.push("Response received at: <i>" + dt.getMinutes() + ":" + dt.getSeconds() + ":" + dt.getMilliseconds() + "</i>");
+                            //console.log(response);
+                            console.log("Step 1.2");
+                            console.log(!err);
+
+                            if (!err) {
+                                console.log("Step 2");
+                                console.log(JSON.stringify(response.json.rows[0].elements));
+                                response.json.rows[0].elements.forEach(function (item, index) {
+                                    results.push(item);
+                                });
+                                //results.push();
+                                callback();
+                            }
+                        });
+                    });
+                }(destinations));
+                i = i + MAX_DESTINATIONS_PER_REQUEST;
+            }
 
             async.parallel(asyncTasks, function(){
-                response.end(JSON.stringify(results));
+                console.log("Step 5");
+                timestamps.push("Total time taken in ms : <i>" + (new Date().getTime() - totalTimeInMS) + "</i>");
+                timestamps.push("<br /><br />Total destinations received: <i>" + all_destinations.length + "</i>");
+                timestamps.push("Max destinations per API call: <i>" + MAX_DESTINATIONS_PER_REQUEST+"</i>");
+
+
+                response.end(JSON.stringify({"results": results, "timestamps": timestamps}));
             });
 
         });
     }
+    //response.write("Step 3");
+    console.log("Step 3");
 
     //response.end("Hello Js");
 
 }
+
 
 const server = http.createServer(requestHandler)
 
